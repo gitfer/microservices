8add5cdbbd8eeeef58400dc787cf8720f875db1c
Federico Rota <federico.rota01@gmail.com>
2018-07-22 23:51:06 +0200

    Adds link to mvc controller

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -1,38 +1,43 @@
-﻿import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { Glyphicon, Nav, Navbar, NavItem } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
-import './NavMenu.css';
+﻿import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Glyphicon, Nav, Navbar, NavItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import "./NavMenu.css";
 
 export class NavMenu extends Component {
-  displayName = NavMenu.name
+  displayName = NavMenu.name;
 
   render() {
     return (
       <Navbar inverse fixedTop fluid collapseOnSelect>
         <Navbar.Header>
           <Navbar.Brand>
-            <Link to={'/'}>microservices</Link>
+            <Link to={"/"}>microservices</Link>
           </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav>
-            <LinkContainer to={'/'} exact>
+            <LinkContainer to={"/"} exact>
               <NavItem>
-                <Glyphicon glyph='home' /> Home
+                <Glyphicon glyph="home" /> Home
               </NavItem>
             </LinkContainer>
-            <LinkContainer to={'/counter'}>
+            <LinkContainer to={"/counter"}>
               <NavItem>
-                <Glyphicon glyph='education' /> Counter
+                <Glyphicon glyph="education" /> Counter
               </NavItem>
             </LinkContainer>
-            <LinkContainer to={'/fetchdata'}>
+            <LinkContainer to={"/fetchdata"}>
               <NavItem>
-                <Glyphicon glyph='th-list' /> Fetch data
+                <Glyphicon glyph="th-list" /> Fetch data
               </NavItem>
             </LinkContainer>
+            <li role="presentation" class="">
+              <a href="/helloworld">
+                <span class="glyphicon glyphicon-th-list" /> Hello world
+              </a>
+            </li>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
